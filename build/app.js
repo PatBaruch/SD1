baba1232c85e2223dea5c1419652b06d5a0e3238
PatBaruch <baru0001@hz.nl>
2024-12-10 19:08:25 +0100

    added git ignore

--- build/app.js
+++
@@ -1,6 +0,0 @@
-import WizardsChest from './VirusVanguard.js';
-const game = new WizardsChest(document.getElementById('game'));
-window.addEventListener('load', () => {
-    game.start();
-});
-//# sourceMappingURL=app.js.map
